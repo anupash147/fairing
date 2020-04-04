96273287a0bf74afb2e1cbf222c610a014dc4223
aash <aash@monsanto.com>
2020-04-04 11:48:17 -0500

    some more debugs
    Signed-off-by: aash <aash@monsanto.com>

--- containerregistry/client/v2_2/docker_session_.py
+++ containerregistry/client/v2_2/docker_session_.py
@@ -66,6 +66,7 @@ class Push(object):
       ValueError: an incorrectly typed argument was supplied.
     """
     self._name = name
+    print("Docker registry name ->",self._name.registry)
     self._transport = docker_http.Transport(name, creds, transport,
                                             docker_http.PUSH)
     self._mount = mount
